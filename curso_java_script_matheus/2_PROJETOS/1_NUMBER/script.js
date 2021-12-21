6bc426edf6406f50b2165728a62df3c973ae0b2d
MacielDev <andreraizdetudo@gmail.com>
2021-12-21 16:07:20 -0300

    Avançando na revisão com tipos de dados

---
+++ curso_java_script_matheus/2_PROJETOS/1_NUMBER/script.js
@@ -0,0 +1,13 @@
+/*UTILIZANDO5 A função 'typeof'*/
+
+/*Imprimindo os números*/
+console.log(5)
+console.log(1.3)
+console.log(-5)
+
+/*VERIFICANDO O TIPO DE DADOS*/
+console.log(typeof 5)   //NUMBER
+console.log(typeof 1.3) //NUMBER
+console.log(typeof -5) //NUMBER
+
+// NOTE QUE NO JAVA SCRIPT NÃO EXISTE DIFERENCIAÇÃO ENTRE TIPOS DE NÚMEROS , TODOS SÃO TRATADOS SIMPLESMENTE COMO NUMBERS
