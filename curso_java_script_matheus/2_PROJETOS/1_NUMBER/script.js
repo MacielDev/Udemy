70135008b86c5632b195bfaf3e85bcae8949b54c
MacielDev <andreraizdetudo@gmail.com>
2021-12-22 14:05:58 -0300

    Especial Numbers

--- curso_java_script_matheus/2_PROJETOS/1_NUMBER/script.js
+++ curso_java_script_matheus/2_PROJETOS/1_NUMBER/script.js
@@ -4,11 +4,12 @@
 console.log(5)
 console.log(1.3)
 console.log(-5)
+console.log(6/0) /*resultado será : infinity,*/
 
 /*VERIFICANDO O TIPO DE DADOS*/
 console.log(typeof 5)   //NUMBER
 console.log(typeof 1.3) //NUMBER
 console.log(typeof -5) //NUMBER
-
+console.log(typeof(6/0))
 
 // NOTE QUE NO JAVA SCRIPT NÃO EXISTE DIFERENCIAÇÃO ENTRE TIPOS DE NÚMEROS , TODOS SÃO TRATADOS SIMPLESMENTE COMO NUMBERS
