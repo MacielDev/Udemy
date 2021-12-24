31d9545c35efe699059a0ec70dac35d8517ea5b5
MacielDev <andreraizdetudo@gmail.com>
2021-12-24 06:25:27 -0300

    até aula 57

---
+++ curso_java_script_matheus/Secao_4 _Estruturas_de_programacao/Aulas/aula57/script.js
@@ -0,0 +1,9 @@
+let nome = "Andre";
+let idade =36
+if(nome != undefined && nome == "joaquim"){
+    console.log("Nome está definido")
+}else if(nome == "Andre" && nome.length >= 5 && idade ==55){
+    console.log("O nome é André")
+}else{
+    console.log("Não é André")
+}
