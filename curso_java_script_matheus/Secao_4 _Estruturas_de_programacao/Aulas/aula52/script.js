c55648c66dffb384bff3709af7a95b14a10fabbb
MacielDev <andreraizdetudo@gmail.com>
2021-12-23 21:52:21 -0300

    segundo exercicio

---
+++ curso_java_script_matheus/Secao_4 _Estruturas_de_programacao/Aulas/aula52/script.js
@@ -0,0 +1,20 @@
+let maior = Math.max(6,12,28,55)
+console.log(maior)
+
+let menor = Math.min(6,12,28,55)
+console.log(menor)
+
+/*Respeita a regra de arredondamento */
+let arredondar1 = Math.round(4.6)
+console.log(arredondar1)
+
+let arredondar2 = Math.round(4.2)
+console.log(arredondar2)
+
+/*Força o arredondamento para cima, independente da regra de arredondamento */
+let arredondar3 = Math.ceil(4.2)
+console.log(arredondar3)
+
+/*Força o arredondamento para baixo, independente da regra de arredondamento */
+let arredondar4 = Math.floor(4.6)
+console.log(arredondar4)
