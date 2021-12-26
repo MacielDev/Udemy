c21e7b76ab0e5e6c06bed09598e91d6a5ce07cdf
MacielDev <andreraizdetudo@gmail.com>
2021-12-25 22:09:33 -0300

    aula 105

--- curso_java_script_matheus/secao6-funcoes/aulas/Aula104-clousure/script.js
+++ curso_java_script_matheus/secao6-funcoes/aulas/Aula104-clousure/script.js
@@ -5,7 +5,24 @@ function lembrarSoma (x) {
     }
 }
 let soma1 = lembrarSoma(2)
-console.log(soma(5))
+console.log(soma1(5))
 
 let soma2 = lembrarSoma(5)
-console.log(soma(7))
+console.log(soma2(7))
+
+console.log("**********************************")
+console.log("******* Exemplo Contador *********")
+
+    function contador(i) {
+        let cont = i
+        let somarContador = function(){
+            console.log(cont)
+            cont++
+        }
+        return somarContador
+        
+    }
+    let meuContador = contador(5)
+    meuContador()
+    meuContador()
+    meuContador()
