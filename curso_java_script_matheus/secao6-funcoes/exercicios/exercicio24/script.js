c62f16ab868ad2c7e0e0af49e94862004b310cad
MacielDev <andreraizdetudo@gmail.com>
2021-12-27 18:41:14 -0300

    Finalizando a seção 7

--- curso_java_script_matheus/secao6-funcoes/exercicios/exercicio24/script.js
+++ curso_java_script_matheus/secao6-funcoes/exercicios/exercicio24/script.js
@@ -1,14 +1,1 @@
-// Calculando a potencia de um número sem utilizar a classe Math e o método pow(BASE, EXPOENTE)
-
-function calculaPotenciaNaRaca(bas, exp) {
-    let resultado = 1
-    for (let i = 0; i == exp; i++) {
-        if (bas != 0 && exp == 0) {
-            return resultado
-        }
-        resultado = resultado * bas
-    }
-    return resultado
-}
-
-console.log(calculaPotenciaNaRaca(2, 1))
+console.log(Math.pow(2, -1))
---
+++ curso_java_script_matheus/secao6-funcoes/exercicios/exercicio25/script.js
@@ -0,0 +1,10 @@
+function decrementa(numero) {
+    while (numero >= 0) {
+        if (numero % 2 == 0) {
+            console.log(numero)
+        }
+        numero--
+    }
+}
+
+decrementa(13)
