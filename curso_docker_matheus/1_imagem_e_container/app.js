964caefab85bc4e77e30c42f2b9700700a1a01d1
MacielDev <andreraizdetudo@gmail.com>
2021-12-30 22:47:54 -0300

    alterarando imagem e dando build novamente

--- curso_docker_matheus/1_imagem_e_container/app.js
+++ curso_docker_matheus/1_imagem_e_container/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const port =3000;
-app.get('/', (req, res) =>{
-    res.send('Olá minha imagem está rodando');
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Olá minha imagem !');
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Executando na porta ${port}`);
 });
